ce4abedd8ae9cf4911cda1a9ae1df732ab91df66
Vinícius Matteus <vinicius.matteus@appmax.com.br>
2024-11-19 19:34:45 -0300

    fiz o metodo para deletar o node

--- tree.php
+++ tree.php
@@ -7,35 +7,40 @@ class Node
     private $left;
     private $right;
 
-    public function __construct(int $value = null, Node $left = null, Node $right = null)
+    public function __construct(int $value)
     {
         $this->value = $value;
-        $this->left = $left;
-        $this->right = $right;
+        $this->left = null;
+        $this->right = null;
     }
 
-    public function getValue() 
+    public function getValue(): int
     {
         return $this->value;
     }
 
-    public function getLeft() 
+    public function setValue(int $value): void
+    {
+        $this->value = $value;
+    }
+
+    public function getLeft(): Node | null
     {
         return $this->left;
     }
 
-    public function getRight() 
+    public function getRight(): Node | null
     {
         return $this->right;
     }
 
-    public function setLeft(Node $node) 
+    public function setLeft(Node $node = null): Node
     {
         $this->left = $node;
         return $this;
     }
 
-    public function setRight(Node $node)
+    public function setRight(Node $node = null): Node
     {
         $this->right = $node;
         return $this;
@@ -51,15 +56,22 @@ class BinarySearchTree
         $this->root = $node;
     }
 
-    public function insertValue(int $value): Node {
+    public function insertValue(int $value): Node 
+    {
         return $this->addNode($this->root, $value);
     }
 
-    public function searchValue(int $value): bool {
+    public function searchValue(int $value): bool
+    {
         return $this->searchNode($this->root, $value);
     }
 
-    public function getRoot() 
+    public function deleteValue(int $value): Node | null
+    {
+        return $this->deleteNode($this->root, $value);
+    }
+
+    public function getRoot(): Node
     {
         return $this->root;
     }
@@ -95,6 +107,64 @@ class BinarySearchTree
         if ($value > $node->getValue()) {
             return $this->searchNode($node->getRight(), $value);
         }
+    }
+
+    private function findMinValue(Node $node): int
+    {
+        $current = $node;
+        
+        while ($current->getLeft() !== null) {
+            $current = $current->getLeft();
+        }
+        return $current->getValue();
+    }
+
+    private function deleteNode(Node $node = null, int $value): Node | null
+    {
+        if (is_null($node)) {
+            return null;
+        }
+
+        $nodeLeft = $node->getLeft();
+        $nodeRight = $node->getRight();
+
+        if (is_null($nodeLeft) && is_null($nodeRight)) {
+            return null;
+        }
+
+        if ($value < $node->getValue()) {
+            $nodeReturn = $this->deleteNode($nodeLeft, $value);
+            
+            if ( is_null($nodeReturn) ) {
+                $node->setLeft(null);
+            }
+
+            return $node;
+        }
+
+        if ($value > $node->getValue()) {
+            $nodeReturn = $this->deleteNode($nodeRight, $value);
+
+            if ( is_null($nodeReturn) ) {
+                $node->setRight(null);
+            }
+
+            return $node;
+        }
+
+        if (is_null($nodeLeft)) {
+            return $nodeRight;
+        }
+
+        if (is_null($nodeRight)) {
+            return $nodeLeft;
+        }
+
+        $minValue = $this->findMinValue($nodeRight);
+        $node->setValue($minValue);
+        $node->setRight($this->deleteNode($nodeRight, $minValue));
+        
+        return $node;
     }
 }
 
@@ -146,3 +216,8 @@ $tree->insertValue(200);
 
 drawTree($tree->getRoot());
 var_dump($tree->searchValue(201));
+
+$tree->deleteValue(200);
+// $tree->deleteValue(5);
+
+drawTree($tree->getRoot());
